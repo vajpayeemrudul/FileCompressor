9f623929e3b75aa8af3e7d798baee9058695f509
vajpayeemrudul <mrudulrcoem@gmail.com>
2022-03-16 08:06:57 +0530

    update upload component

--- src/Components/Upload/Upload.js
+++ src/Components/Upload/Upload.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Upload() {
   return (
     <div className="upload">
-        <h1>Hi I am upload</h1>
+        <h2>Upload the file to Compress</h2>
+        <form>
+            <label for="myfile">Select your file: </label>
+            <input type="file" id="myfile" name="myfile"/>
+            <br/><br/>
+            <input type="submit"/>
+        </form>
     </div>
   )
 }
