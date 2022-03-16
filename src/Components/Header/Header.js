479136b600a8b0a4473c9668a17de69819982175
vajpayeemrudul <mrudulrcoem@gmail.com>
2022-03-16 07:44:46 +0530

    Make file structure and basic UI

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="head">
+        <h1>COMPRESSORS</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Upload/Upload.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Upload() {
+  return (
+    <div className="upload">
+        <h1>Hi I am upload</h1>
+    </div>
+  )
+}
+
+export default Upload
